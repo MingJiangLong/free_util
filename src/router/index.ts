e9de50185606127d21c898f3d04aefbe97899e57
longjiang <121874694@qq.com>
2022-01-05 18:33:44 +0800

    增加prefix前缀

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import ImageUtilRouter from '../controller/ImageUtilController';
 import RobotRouter from '../controller/RobotUtilController';
 import XYGDRouter from '../controller/XYGDController';
 const routerManager = new Router();
-
+routerManager.prefix('/api')
 const routerArr = [
     CmdRouter,
     ImageUtilRouter,
